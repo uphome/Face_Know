42e1fee5212a10aa1326f91a879ff09e0ea942ce
uphome <1822358119@qq.com>
2022-12-30 03:20:38 +0800

    完成了人脸识别以及配准的部分，并添加了进度条，同时美化了终端

--- video.py
+++ video.py
@@ -1,21 +1,34 @@
 import cv2
 import time
+from tqdm import tqdm
 import os
 import numpy as np
+import sys
+
 #
 #
 #
 ####相关设置####
 Name = "FENG"
 name_set = False  # Ture 为保存脸部信息 False 为不保存脸部信息
-data_path='/home/hu/PycharmProjects/Face_know/Face_data/'  #脸部数据存储位置
+data_path = '/home/hu/PycharmProjects/Face_know/Face_data/'  # 脸部数据存储位置
+
+
 ########
+
+def time_kown(times):
+    print("\r", end="")
+    print("『INFO』进度: {}%: ".format(times), "▓" * (times // 2), end="")
+    sys.stdout.flush()
+    time.sleep(0.05)
 
 
 #################################
 number_video = 0
 count = 0
-image_dit=[]
+image_dit = []
+labels = []
+name_list = {}
 url = "http://admin:123456@192.168.69.133:8081"
 # video="/home/hu/桌面/home/hu/桌面/VID20221229180719.mp4"
 
@@ -24,16 +37,40 @@ url = "http://admin:123456@192.168.69.133:8081"
 face_cascade = cv2.CascadeClassifier('/home/hu/桌面/haarcascade_frontalface_alt.xml')
 face_cascade.load('/home/hu/桌面/haarcascade_frontalface_alt.xml')
 
-print('start')
-print("是否保存人脸信息：" + str(name_set) + '\n')
+print('『INFO』start')
+print("『INFO』是否保存人脸信息：" + str(name_set) + '\n')
 if name_set:
     print("姓名为" + Name)
-    os.mkdir(str(data_path+Name))
-else:
+    os.mkdir(str(data_path + Name))
+else:  # 构造原始人脸数据库   以及人脸标签库
     Name_arry = os.listdir(data_path)
-    for i in Name_arry:
-        for j in data_path+
+    # print(len(Name_arry))
+    for i in range(len(Name_arry)):
+        name_list[Name_arry[i]] = i + 1
 
+    for i in Name_arry:
+        for j in os.listdir(str(data_path + i)):
+            # print(data_path+i+'/'+j)
+            image_dit.append(cv2.imread(str(data_path + i + "/" + j), cv2.IMREAD_GRAYSCALE))
+
+            labels.append(name_list[i])
+
+# ! 用现有的数据集训练识别器
+print("『INFO』开始训练选择器")
+time_kown(0)
+recognizer_a = cv2.face.EigenFaceRecognizer_create()  ## EigenFace(PCA，5000以下判断可靠）
+recognizer_a.train(image_dit, np.array(labels))  ##  这里只能是整数哟
+time_kown(33)
+print("  EigenFace 训练完成")
+recognizer_b = cv2.face.LBPHFaceRecognizer_create()  # LBPH（局部二值模式直方图，0完全匹配，50以下可接受，80不可靠
+recognizer_b.train(image_dit, np.array(labels))
+time_kown(66)
+print("  LBPH 训练完成")
+recognizer_c = cv2.face.FisherFaceRecognizer_create()  # Fisher(线判别分析 ， 5000以下判断为可靠）
+recognizer_c.train(image_dit, np.array(labels))
+time_kown(100)
+print("  Fisher 训练完成")
+print("『INFO』选择器训练完成！")
 
 # ! 开始运行
 cap = cv2.VideoCapture(url)  # 读取视频流    1280*720
@@ -54,7 +91,8 @@ while cap.isOpened():
         # 原始数据返回的是正方形
         f = cv2.resize(image_gray[y:y + h, x:x + w], (200, 200))  # 脸部图片
         # cv2.imshow("f",f)
-        print("\033[1;44m 识别成功 \033[1;0m")
+        if name_set == False:
+        print("\033[1;44m 『INFO』识别成功 \033[1;0m")
         cv2.imshow("camera", img)
         number = 1
         # 保存脸部图片   可选是否保存脸部数据
@@ -62,15 +100,29 @@ while cap.isOpened():
             if ret:
                 cv2.imwrite("/home/hu/PycharmProjects/Face_know/Face_data/" + Name + '/%s.png' % str(count), f)
                 count = count + 1
+        else:
+            labels_a, correct_num_a = recognizer_a.predict(f)
+        labels_b, correct_num_b = recognizer_b.predict(f)
+        labels_c, correct_num_c = recognizer_c.predict(f)
+        # print(labels_a,labels_b,labels_c)
+        labels = [labels_a, labels_b, labels_c]
+        Labels = max(labels, key=labels.count)
+        # print(correct_num_a, correct_num_b, correct_num_c)
+        correct_num_a = (correct_num_a - 5000)
+        correct_num_b = (correct_num_b - 50)
+        correct_num_c = (correct_num_c - 5000)
+        # print(correct_num_a,correct_num_b,correct_num_c)
+        print("\033[1;44m 『INFO』识别为 \033[1;0m" + "\033[1;42m%s\033[1;0m" % list(name_list.keys())[
+            list(name_list.values()).index(Labels)])
+        # TODO 向视频流中添加文字
+
         # 展示图片
     if number == 0:
-        print("\033[1;41m 无法识别 \033[1;0m")
+        print("\033[1;41m 『INFO』无法识别 \033[1;0m")
         cv2.imshow('camera', frame)
 
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
 
-
 cap.release()
 cv2.destroyAllWindows()
-
